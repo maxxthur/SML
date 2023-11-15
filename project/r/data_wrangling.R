8f83f79c7d9eb13f27a160e72c7a05116d70ec3e
maxxthur <46267997+maxxthur@users.noreply.github.com>
2023-11-15 15:09:30 +0100

    converted categorical data to factors

--- project/r/data_wrangling.R
+++ project/r/data_wrangling.R
@@ -1,4 +1,4 @@
-pacman::p_load("tidyverse", "janitor")
+pacman::p_load("tidyverse", "janitor", "naniar", "ggthemes", "xtable", "patchwork")
 
 
 # Load data ---------------------------------------------------------------
@@ -48,15 +48,150 @@ data_prel <- raw_data %>%
   mutate(travel_group = substr(PassengerId, 1, 4)) %>% 
   group_by(travel_group) %>% 
   mutate(travel_group_size = n()) %>% 
-  ungroup()
+  ungroup() %>% 
+  mutate(Cabin_Number = as.integer(Cabin_Number))
 
 
 # Exploratory Data Analysis -----------------------------------------------
 
 # plot target distribution
+data_prel %>% 
+  ggplot(aes(x = Transported, fill = Transported)) +
+  scale_fill_manual(values = c("steelblue", "orange")) +
+  geom_bar(stat = "count") +
+  theme_classic() +
+  theme(legend.position = "none")
+
+ggsave("project/plots/target.jpg", width = 96, height = 72, units = "mm")
+
+# plot of age
+data_prel %>% 
+ggplot(aes(x = Age)) +
+  scale_color_manual(values = c("steelblue", "orange")) +
+  scale_fill_manual(values = c("steelblue", "orange")) +
+  geom_histogram(aes(fill = Transported, color = Transported), 
+                 alpha=0.6, position = "identity", binwidth = 1) +
+  theme_classic() +
+  theme(legend.position = "bottom", 
+        legend.title = element_blank())
+
+ggsave("project/plots/age.jpg", width = 96, height = 72, units = "mm")
+
+# plots for features related to booking details
+data_prel %>% 
+  select(HomePlanet, CryoSleep, VIP, Destination, Transported) %>% 
+  mutate(Destination = case_when(
+    str_detect(Destination, "55") ~ "C",
+    str_detect(Destination, "22") ~ "PSO",
+    str_detect(Destination, "1e") ~ "T"
+  )) %>%
+  mutate(across(.cols = everything(), ~ as.character(.x))) %>% 
+  pivot_longer(cols = - Transported) %>% 
+  filter(value != "NA") %>% 
+  ggplot(aes(x = value, fill = as.logical(Transported))) +
+  scale_fill_manual(values = c("steelblue", "orange")) +
+  geom_bar(position = "dodge") +
+  facet_wrap(~name, scales = "free") +
+  theme_classic() +
+  theme(legend.position = "bottom", 
+        legend.title = element_blank()) +
+  labs(x = "", y = "")
+
+ggsave("project/plots/booking.jpg", width = 96, height = 72, units = "mm")
 
 
+# location on spaceship
+data_prel %>% 
+  ggplot(aes(x = Cabin_Number, fill = Transported)) +
+  scale_color_manual(values = c("steelblue", "orange")) +
+  scale_fill_manual(values = c("steelblue", "orange")) +
+  geom_histogram(position = "identity", alpha = 0.6, binwidth = 10) +
+  theme_classic() +
+  labs(x = "Cabin Number") + 
+  theme(legend.position = "none")
 
+ggsave("project/plots/cabin.jpg", width = 96, height = 72, units = "mm")
+
+plot_deck <- data_prel %>% 
+  filter(Deck != "NA") %>% 
+  ggplot(aes(x = Deck, fill = Transported)) +
+  scale_fill_manual(values = c("steelblue", "orange")) +
+  geom_bar(position = "dodge") +
+  theme_classic() +
+  theme(legend.position = "none")
+
+ggsave("project/plots/deck.jpg", width = 96, height = 72, units = "mm")
+
+plot_side <- data_prel %>% 
+  filter(Deck != "NA") %>% 
+  ggplot(aes(x = Side, fill = Transported)) +
+  scale_fill_manual(values = c("steelblue", "orange")) +
+  geom_bar(position = "dodge") +
+  theme_classic()
+
+ggsave("project/plots/side.jpg", width = 96, height = 72, units = "mm")
+
+  
+  
+# plot for extremely skewed  expenditure features - use log values
+data_prel %>% 
+  select(Transported, RoomService, FoodCourt, ShoppingMall, Spa, VRDeck, TotalExpenditure= total_expenditure) %>% 
+  pivot_longer(cols = -Transported) %>% 
+  ggplot(aes(x = log(value+1))) +
+  scale_color_manual(values = c("steelblue", "orange")) +
+  scale_fill_manual(values = c("steelblue", "orange")) +
+  geom_histogram(aes(fill = Transported, color = Transported), 
+                 alpha=0.6, position = "identity") +
+  facet_wrap(~name) +
+  theme_classic() +
+  theme(legend.title = element_blank(),
+        legend.position = "none")
+
+ggsave("project/plots/expenditure.jpg", width = 96, height = 72, units = "mm")
+
+# plot for engineered features
+data_prel %>% 
+  filter(!is.na(family)) %>% 
+  select(family_size, travel_group_size, Transported) %>% 
+  pivot_longer(cols = -Transported) %>% 
+  ggplot(aes(x = value, fill = Transported)) +
+  geom_bar(position = "dodge") +
+  facet_wrap(~name, nrow = 2)
+
+# heatmap of missing values
+data_prel %>% 
+  select(-Age_Group, -total_expenditure, -any_expenditure, -family, -family_size, 
+         -travel_group, -travel_group_size) %>% 
+  mutate(across(.cols = everything(), ~ is.na(.x))) %>% 
+  mutate(row = row_number()) %>% 
+  pivot_longer(cols = -row) %>% 
+  ggplot(aes(x = name, y = row, fill = value)) +
+    geom_tile() +
+  labs(fill = "Missing", x = "") +
+  scale_fill_manual(values = c("darkgreen", "orange")) +
+  coord_flip() +
+  theme(panel.border = element_blank(), 
+        panel.background = element_blank(),
+        legend.position = "bottom")
+  
+  ggsave("project/plots/missings.png", width = 96, height = 72, units = "mm")
+  
+data_prel %>% 
+  select(-Age_Group, -total_expenditure, -any_expenditure, -family, -family_size, 
+         -travel_group, -travel_group_size) %>% 
+  mutate(across(.cols = everything(), ~ is.na(.x))) %>% 
+  summarise(across(.cols = everything(), 
+                   ~ mean(.x))) %>% 
+  pivot_longer(cols = everything(), 
+               names_to = "Variable", 
+               values_to = "Share Missing") %>% 
+  xtable()
+  
+  
+  
+  
+    
+  
 
 
 # Impute NA ---------------------------------------------------------------
@@ -103,10 +238,42 @@ data_imp <- raw_data%>%
   mutate(travel_group_size = n()) %>% 
   ungroup()
 
+test <- sample(1:nrow(data_imp), 0.25 * nrow(data_imp))
 
-data_imp %>% 
-  write_csv(file = "project/r/modeling/final data/data.csv")
+set.seed(123)
+dat_test <- data_imp[test, ]
+dat_train <- data_imp[-test, ]
 
+# save 
+write_csv(dat_test, "project/r/modeling/final data/test.csv")
+write_csv(dat_train, "project/r/modeling/final data/train.csv")
 
+# Modeling ----------------------------------------------------------------
+library(randomForest)
+library(caret)
+
+control <- trainControl(method = 'repeatedcv',
+                        number = 10,
+                        repeats = 3,
+                        search = 'grid')
+#create tunegrid
+tunegrid <- expand.grid(.mtry = c(sqrt(ncol(dataset))))
+modellist <- list()
+
+#train with different ntree parameters
+
+train <- dat_train %>% 
+  select(Transported, total_expenditure) %>% 
+
+  fit <- train(Transported~.,
+               data = train,
+               method = 'rf',
+               metric = 'Accuracy',
+               tuneGrid = tunegrid,
+               trControl = control)
+
+#Compare results
+results <- resamples(modellist)
+summary(results)
 
   
